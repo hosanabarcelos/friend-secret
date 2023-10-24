a62b9c461100f876cb27ac951c3d0f7496ec80b3
Hosana <hosanabarceloscandido@gmail.com>
2023-10-24 15:41:22 -0300

    test: add teste para add participante

--- src/components/tests/Form.test.tsx
+++ src/components/tests/Form.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Form from '../Form';
-
-// Jest
+import { RecoilRoot } from 'recoil';
 
 test('if the input is empty, do not accept new participants added', () => {
     render(<Form />);
@@ -18,4 +17,34 @@ test('if the input is empty, do not accept new participants added', () => {
 
     //garantir que o botão esteja desabilitado
     expect(button).toBeDisabled();
+});
+
+test('if the name is filled in, add participant', () => {
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
+
+    //encontrar o DOM no input
+    const input = screen.getByPlaceholderText('Insira o nome do participante');
+
+    //encontrar o botão
+    const button = screen.getByRole('button');
+
+    //inserir valor de input
+    fireEvent.change(input, {
+        target: {
+            value: 'Pedro Astronauta'
+        }
+    });
+
+    //submeter formulário
+    fireEvent.click(button);
+
+    //garantir que o input esteja com foco ativo
+    expect(input).toHaveFocus();
+
+    //garantir que o input não tenha um valor
+    expect(input).toHaveValue('');
 });
