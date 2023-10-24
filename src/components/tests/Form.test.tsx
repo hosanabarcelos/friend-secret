401d278b186c31a15eb27109afa29dc4a4eb1e07
Hosana <hosanabarceloscandido@gmail.com>
2023-10-24 16:08:04 -0300

    test: add casos para nome duplicado e remoção de erro

--- src/components/tests/Form.test.tsx
+++ src/components/tests/Form.test.tsx
@@ -4,7 +4,11 @@ import Form from '../Form';
 import { RecoilRoot } from 'recoil';
 
 test('if the input is empty, do not accept new participants added', () => {
-    render(<Form />);
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
 
     //encontrar o DOM no input
     const input = screen.getByPlaceholderText('Insira o nome do participante');
@@ -47,4 +51,80 @@ test('if the name is filled in, add participant', () => {
 
     //garantir que o input não tenha um valor
     expect(input).toHaveValue('');
+});
+
+test('if the name is requested, do not add and display error', () => {
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
+
+    //encontrar o DOM no input
+    const input = screen.getByPlaceholderText('Insira o nome do participante');
+
+    //encontrar o botão
+    const button = screen.getByRole('button');
+
+    //inserir valor de input
+    fireEvent.change(input, {
+        target: {
+            value: 'Pedro Astronauta'
+        }
+    });
+
+    //Repetir valor de input
+    fireEvent.change(input, {
+        target: {
+            value: 'Pedro Astronauta'
+        }
+    });
+
+    //submeter formulário
+    fireEvent.click(button);
+
+    const errorMessage = screen.getByRole('alert');
+
+    expect(errorMessage.textContent).toBe('Nomes duplicados não são permitidos.')
+});
+
+test('if 3 minutes pass, remove error', () => {
+    jest.useFakeTimers();
+
+    render(
+        <RecoilRoot>
+            <Form />
+        </RecoilRoot>
+    );
+
+    //encontrar o DOM no input
+    const input = screen.getByPlaceholderText('Insira o nome do participante');
+
+    //encontrar o botão
+    const button = screen.getByRole('button');
+
+    //inserir valor de input
+    fireEvent.change(input, {
+        target: {
+            value: 'Pedro Astronauta'
+        }
+    });
+
+    //Repetir valor de input
+    fireEvent.change(input, {
+        target: {
+            value: 'Pedro Astronauta'
+        }
+    });
+
+    //submeter formulário
+    fireEvent.click(button);
+
+    let errorMessage = screen.queryByRole('alert');
+    expect(errorMessage).toBeInTheDocument();
+
+    jest.runAllTimers();
+
+    errorMessage = screen.queryByRole('alert');
+    expect(errorMessage).toBeNull();
 });
