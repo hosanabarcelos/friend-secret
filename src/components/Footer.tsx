5beaa00c6f663d8c3c41d40e64d2e2894cc8650b
Hosana <hosanabarceloscandido@gmail.com>
2023-10-24 16:57:52 -0300

    feat: add lógica de rota

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { useParticipantsList } from "./hooks/useParticipantsList";
 
 const Footer = () => {
 
     const participants = useParticipantsList();
 
+    const navigationTo = useNavigate();
+
+    const start = () => {
+        navigationTo('/sorteio');
+    }
+
     return (
         <footer>
-            <button disabled={participants.length < 3}>Play</button>
+            <button disabled={participants.length < 3} onClick={start}>Play</button>
         </footer>
     )
 }
