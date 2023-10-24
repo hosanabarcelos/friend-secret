50795d8e271575a62586701dab29c192871b278b
Hosana <hosanabarceloscandido@gmail.com>
2023-10-24 15:41:46 -0300

    feat: ajusta lógica com hook para formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,30 @@
+import { useRef, useState } from "react";
+import { useAddParticipant } from "./hooks/addParticipant";
+
 const Form = () => {
+    const [name, setName] = useState('');
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const add = useAddParticipant();
+
+    const addParticipant = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        add(name);
+        setName('');
+        inputRef.current?.focus();
+    }
+
     return (
-        <form>
+        <form onSubmit={addParticipant }>
             <input
+                ref={inputRef}
+                value={name}
+                onChange={event => setName(event.target.value)}
                 type="text"
                 placeholder="Insira o nome do participante"
             />
-            <button disabled={true}>Adicionar</button>
+            <button disabled={!name}>Adicionar</button>
         </form>
     )
 }
